c8ecbd64877f3bcf07af54c63538fbc8d53c4613
dreik84 <dreik84@mail.ru>
2025-10-23 16:51:56 +0300

    Create Nutrition builder

---
+++ src/main/java/org/example/creational/builder/nutrition/NutritionApp.java
@@ -0,0 +1,11 @@
+package org.example.creational.builder.nutrition;
+
+public class NutritionApp {
+    public static void main(String[] args) {
+
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+
+        System.out.println(cocaCola);
+    }
+}
