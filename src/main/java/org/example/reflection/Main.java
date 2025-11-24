c70c1186b4eab9955f5918f08f62f786a55fe46c
dreik84 <dreik84@mail.ru>
2025-11-24 19:26:50 +0300

    Update reflection api

--- src/main/java/org/example/reflection/Main.java
+++ src/main/java/org/example/reflection/Main.java
@@ -2,6 +2,7 @@ package org.example.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -34,10 +35,35 @@ public class Main {
         }
     }
 
+    public static Test createTestObjectWithValue(String value) {
+        try {
+            Constructor<Test> constructor = Test.class.getDeclaredConstructor(String.class);
+            constructor.setAccessible(true);
+            return constructor.newInstance(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void callPrivateMethod(Object object, String methodName, String parameter) {
+        Class<?> aClass = object.getClass();
+
+        try {
+            Method method = aClass.getDeclaredMethod(methodName, String.class);
+            method.setAccessible(true);
+            method.invoke(object, parameter);
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        var test = new Test();
+        var test = createTestObjectWithValue("initValue");
+        System.out.println(test);
         getClassInfo(test);
         setPrivateValue(test, "value", "newValue");
+        System.out.println(test);
+        callPrivateMethod(test, "setValue", "privateValue");
         System.out.println(test);
     }
 }
